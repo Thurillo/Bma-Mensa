29b0d68604f16de303d458fa0721701a613245af
Thurillo <149769367+Thurillo@users.noreply.github.com>
2025-11-02 17:15:36 +0100

    Create apps.py

---
+++ mensa/apps.py
@@ -0,0 +1,13 @@
+"""
+File: apps.py
+Destinazione: mensa/ (Bma-Mensa/mensa/apps.py)
+
+Configurazione dell'app 'mensa'.
+"""
+from django.apps import AppConfig
+
+
+class MensaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mensa'
+
